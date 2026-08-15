a05b54b424d359c9c3ee83ef67f657067610c72c
Jose Luna <josemanuelmluna@gmail.com>
2026-08-15 23:30:09 +0800

    feat: generated event when order is created

--- src/main/java/com/luna/jwt_demo/order/service/OrderService.java
+++ src/main/java/com/luna/jwt_demo/order/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.luna.jwt_demo.order.service;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
+import com.luna.jwt_demo.common.config.RabbitMqConfig;
 import com.luna.jwt_demo.common.exception.custom.ResourceNotFoundException;
 import com.luna.jwt_demo.order.model.OrderDto;
 import com.luna.jwt_demo.order.model.OrderEntity;
@@ -10,14 +12,23 @@ import com.luna.jwt_demo.order.repository.OrderRepository;
 public class OrderService {
 
     private final OrderRepository repository;
+    private final RabbitTemplate rabbitTemplate;
 
-    public OrderService(OrderRepository repository) {
+    public OrderService(OrderRepository repository, RabbitTemplate rabbitTemplate) {
         this.repository = repository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     public void createOrder(OrderDto orderDto) {
         OrderEntity entity = mapDtoToEntity(orderDto);
-        repository.save(entity);
+        OrderEntity savedEntity = repository.save(entity);
+        OrderDto responseDto = mapEntityToDto(savedEntity);
+
+        rabbitTemplate.convertAndSend(
+            RabbitMqConfig.ORDERS_EXCHANGE,
+            RabbitMqConfig.ORDER_CREATED_PATTERN,
+            responseDto
+        );
     }
 
     public OrderDto getOrderById(Long orderId) {
