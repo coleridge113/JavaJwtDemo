536de5af1504d5b9d6c0b1bd28ef362fe78dd5e2
Jose Luna <josemanuelmluna@gmail.com>
2026-08-15 19:43:01 +0800

    refactor: used slf4j logger

--- src/main/java/com/luna/jwt_demo/message/service/MessageConsumerService.java
+++ src/main/java/com/luna/jwt_demo/message/service/MessageConsumerService.java
@@ -1,15 +1,19 @@
 package com.luna.jwt_demo.message.service;
 
 import java.util.Map;
+import org.slf4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import com.luna.jwt_demo.common.config.RabbitMqConfig;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class MessageConsumerService {
 
+    private final static Logger log = LoggerFactory.getLogger(MessageConsumerService.class);
+
     @RabbitListener(queues = RabbitMqConfig.QUEUE_NAME)
     public void receiveMessage(Map<String, String> message) {
-        System.out.println(message);
+        log.info("Message: " + message.get("text"));
     }
 }
