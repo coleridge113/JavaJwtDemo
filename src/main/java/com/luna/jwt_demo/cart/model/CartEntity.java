d9b69012363f2b9d6548fc190553d972bc1071b3
Jose Luna <josemanuelmluna@gmail.com>
2026-08-21 13:37:13 +0800

    feat: connected user into cart entity

--- src/main/java/com/luna/jwt_demo/cart/model/CartEntity.java
+++ src/main/java/com/luna/jwt_demo/cart/model/CartEntity.java
@@ -1,15 +1,20 @@
 package com.luna.jwt_demo.cart.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
+import com.luna.jwt_demo.auth.model.entity.UserInfo;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 
 @Entity
@@ -17,13 +22,36 @@ import jakarta.persistence.Column;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private Long totalAmountInCents;
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false, unique = true)
+    private UserInfo user;
 
-    @OneToMany(mappedBy = "cart")
+    @Column(nullable = false)
+    private Long totalAmountInCents = 0L;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CartItemEntity> items = new ArrayList<>();
+
+    public void addItem(CartItemEntity item) {
+        items.add(item);
+        item.setCart(this);
+        recalculateTotal();
+    }
+
+    public void removeItem(CartItemEntity item) {
+        items.remove(item);
+        item.setCart(null);
+        recalculateTotal();
+    }
+
+    private void recalculateTotal() {
+        totalAmountInCents = items.stream()
+            .mapToLong(item -> item.getAmountInCents() * item.getQuantity())
+            .sum();
+    }
 }
