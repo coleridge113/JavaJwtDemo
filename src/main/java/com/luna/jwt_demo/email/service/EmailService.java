0f524869c167f4cd22b64b30716839ddc3a5a6b0
Jose Luna <josemanuelmluna@gmail.com>
2026-08-25 11:27:22 +0800

    refactor: removed rabbit template

--- src/main/java/com/luna/jwt_demo/email/service/EmailService.java
+++ src/main/java/com/luna/jwt_demo/email/service/EmailService.java
@@ -3,7 +3,6 @@ package com.luna.jwt_demo.email.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import com.luna.jwt_demo.common.config.RabbitMqConfig;
@@ -13,10 +12,6 @@ import com.luna.jwt_demo.order.model.OrderDto;
 public class EmailService {
 
     private static final Logger log = LoggerFactory.getLogger(EmailService.class);
-
-    public EmailService(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
 
     @RabbitListener(queues = RabbitMqConfig.NOTIFICATION_QUEUE)
     public void receiveMessage(OrderDto order) {
