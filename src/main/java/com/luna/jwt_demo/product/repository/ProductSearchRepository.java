705f70ce26562f3830d9d579410325a0f47b902c
Jose Luna <josemanuelmluna@gmail.com>
2026-08-27 21:29:26 +0800

    feat: added paginated product search for ES

--- src/main/java/com/luna/jwt_demo/product/repository/ProductSearchRepository.java
+++ src/main/java/com/luna/jwt_demo/product/repository/ProductSearchRepository.java
@@ -2,6 +2,8 @@ package com.luna.jwt_demo.product.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import com.luna.jwt_demo.product.model.ProductDocument;
@@ -11,5 +13,7 @@ public interface ProductSearchRepository extends ElasticsearchRepository<Product
     List<ProductDocument> findByNameContaining(String name);
 
     List<ProductDocument> findByNameStartingWith(String prefix);
+
+    Page<ProductDocument> findByNameContaining(String keyword, Pageable pageable);
 
 }
