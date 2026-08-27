9454dcd69c09a668b9b6b05564a27e5150ed63ba
Jose Luna <josemanuelmluna@gmail.com>
2026-08-27 21:28:59 +0800

    refactor: made name property multifield to support name sorting

--- src/main/java/com/luna/jwt_demo/product/model/ProductDocument.java
+++ src/main/java/com/luna/jwt_demo/product/model/ProductDocument.java
@@ -4,6 +4,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.InnerField;
+import org.springframework.data.elasticsearch.annotations.MultiField;
 
 import lombok.NoArgsConstructor;
 
@@ -14,7 +16,12 @@ public class ProductDocument {
     @Id
     private String id;
 
-    @Field(type = FieldType.Text, analyzer = "standard")
+    @MultiField(
+        mainField = @Field(type = FieldType.Text, analyzer = "standard"),
+        otherFields = {
+            @InnerField(suffix = "keyword", type = FieldType.Keyword)
+        }
+    )
     private String name;
 
     @Field(type = FieldType.Long)
